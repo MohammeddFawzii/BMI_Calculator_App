aae14dbe45175a10e330685ffb660aa1acaaad46
MohammeddFawzii <fawzimohamed@gmail.com>
2023-04-09 02:37:47 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BmiCalc());
+}
+class BmiCalc extends StatefulWidget {
+  const BmiCalc({super.key});
+
+  @override
+  State<BmiCalc> createState() => _BmiCalcState();
+}
+
+class _BmiCalcState extends State<BmiCalc> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
